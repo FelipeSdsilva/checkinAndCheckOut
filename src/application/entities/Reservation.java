44b26fba139b0d880278995e86333c23c2bac02b
Ubunto Felipe <felipe.fps09@hotmail.com>
2023-01-14 15:29:49 -0300

    Initialize project

---
+++ src/application/entities/Reservation.java
@@ -0,0 +1,51 @@
+package application.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Reservation {
+
+    private Integer roomOfNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    private final static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/HH/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomOfNumber, LocalDate checkIn, LocalDate checkOut) {
+        this.roomOfNumber = roomOfNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomOfNumber() {
+        return roomOfNumber;
+    }
+
+    public void setRoomOfNumber(Integer roomOfNumber) {
+        this.roomOfNumber = roomOfNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration() {
+        LocalDate diff;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "Room= " + roomOfNumber +
+                ", checkIn= " + fmt.format(checkIn) +
+                ", checkOut= " + fmt.format(checkOut) +
+                '}';
+    }
+}
