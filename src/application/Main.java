08ec261db493bca3904ffb323cdc6858e97e3c3b
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-16 18:59:26 -0300

    call method MenuControl.InitMenu()

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,16 @@
 package application;
 
+import application.model.controller.MenuControl;
+import application.model.services.StaticService;
+import application.views.MenuView;
+
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+
+        MenuControl.InitMenu();
     }
 }
