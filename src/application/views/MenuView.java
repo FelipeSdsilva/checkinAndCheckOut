7676afe252c5b8690bc2cfaf90f9bfbc8ba77537
Felipe Sousa <felipe.fps09@hotmail.com>
2023-01-16 19:15:17 -0300

    Add package views, create classes MenuView and TextView

---
+++ src/application/views/MenuView.java
@@ -0,0 +1,39 @@
+package application.views;
+
+public class MenuView {
+
+    public String menuInit = """
+             ___________________________________________________
+            |                                                   |
+            |                                                   |
+            |                                                   |
+            |                                                   |
+            |         Welcome The System of Reservation         |
+            |                  Do you need ?                    |
+            |                                                   |
+            |           1 - Include new reservation             |
+            |           2 - Update date reservation             |
+            |           3 - Remove reservation                  |
+            |           0 - Finish system                       |
+            |                                                   |
+            |                                                   |
+            |                                                   |
+            |_______________________""";
+
+    public String menuEnd = """
+             ___________________________________________________
+            |                                                   |
+            |                                                   |
+            |                                                   |
+            |                                                   |
+            |           Bye The System of Reservation           |
+            |                  Thank you for                    |
+            |                      Using!                       |
+            |                                                   |
+            |                                                   |
+            |                                                   |
+            |                                                   |
+            |                                                   |
+            |_______________________""";
+
+}
---
+++ src/application/views/TextView.java
@@ -0,0 +1,13 @@
+package application.views;
+
+public class TextView {
+
+    public String numberOfReservations = "How many reservations you to go register? ";
+    public String room = "Room number: ";
+    public String checkIn = "Check-In date (dd/MM/yyyy): ";
+    public String checkOut = "Check-Out date (dd/MM/yyyy): ";
+    public String updateCheckInAndCheckOut = "Enter data to update the reservation: ";
+    public String errorInPastDate = "Error in reservation: Reservation dates for update must be future dates";
+    public String errorInCheckOutDate = "Error in reservation: Check-out date must be after check-in date";
+    public String continuationOfRegister = "Do you need to other reservation register(y/n)? ";
+}
